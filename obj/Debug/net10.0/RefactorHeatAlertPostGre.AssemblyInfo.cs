eaf70cf97134f8b33ddc9de6626e144d4a5277ef
kalbashi09 <pauljode1234@gmail.com>
2026-05-11 11:39:20 +0800

    LinuxBaseHeatAlertMigration

--- obj/Debug/net10.0/RefactorHeatAlertPostGre.AssemblyInfo.cs
+++ obj/Debug/net10.0/RefactorHeatAlertPostGre.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefactorHeatAlertPostGre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c067c32a75d84f768b10c48beef72cbd962ccd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e887792d309c7107b3d8ea201bae2421d2484f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefactorHeatAlertPostGre")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefactorHeatAlertPostGre")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
